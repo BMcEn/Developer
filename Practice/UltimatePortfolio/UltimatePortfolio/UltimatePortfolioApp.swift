2b6c59a7507e0d2811056e4ba4f34956faf8ccbb
Ben McEnulty <30420560+BMcEn@users.noreply.github.com>
2021-09-05 14:52:09 -0700

    Initial UltimatePortfolio app commit

---
+++ Practice/UltimatePortfolio/UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Benjamin McEnulty on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
