ee8efe9fcf000f30ab110864f20499a5b259a9e8
Ben McEnulty <30420560+BMcEn@users.noreply.github.com>
2021-09-08 00:34:29 -0700

    iExpense started

---
+++ Practice/100DaysofSwiftUI/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Benjamin McEnulty on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
